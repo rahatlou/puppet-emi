92598862ffe322592d05c6ee7d03a048ea987ddb
talamoig <ivano.talamo@gmail.com>
2015-04-27 16:07:39 +0200

    added installation of yum-protectbase and yum-priorities

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,9 @@
 class emi::install {
+  package{$emi::params::packages_for_all:
+    ensure => installed,
+  }
   package{$emi::params::packages[$emi::profile]:
-    ensure => installed
+    ensure => installed,
   }
   emi::wlcg_voms_package{$emi::vos:}
 }
