bf57d6c5f265022fb7b77a13e5e2e09c94cb4ccb
talamoig <ivano.talamo@gmail.com>
2015-04-27 16:12:24 +0200

    typo correction

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class emi::params {
   $emi_rel_rpm = 'http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/x86_64/base/emi-release-3.0.0-2.el6.noarch.rpm'
   $yaimbin='/opt/glite/yaim/bin/yaim'
   $yaimlog='/opt/glite/yaim/reconfig.log'
-  $packages_for_all ['yum-priorities','yum-protectbase']
+  $packages_for_all = ['yum-priorities','yum-protectbase']
   $profiles = ['ui']
   $packages = {
     ui => ['emi-ui','glite-ce-monitor-cli'],
