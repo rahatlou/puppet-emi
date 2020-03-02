285febbb92abbde4d3c16241c39201196c9c2f8b
rahatlou <rahatlou@gmail.com>
2020-03-02 16:06:14 +0100

    replace emi3 with epel+UMSD4 in my forked version

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -15,11 +15,29 @@ class emi::repo {
     ensure => installed,
   }
   
-  package{"emi-release-3.0.0":
+#
+## ShR 2020-03-02: replace old emi-release with epel + UMD4
+#
+#  package{"emi-release-3.0.0":
+#    ensure   => present,
+#    source   => $emi::params::emi_rel_rpm,
+#    provider => 'rpm',
+#    require => Package[$emi::params::packages_for_all]
+#  }
+
+  $umdrepopkg = "umd-release-4.1.3-1.el6.noarch"
+  package{$umdrepopkg:
+    provider        => 'rpm',
+    install_options => ['-U'],
+    source          => "http://repository.egi.eu/sw/production/umd/4/sl6/x86_64/updates/${umdrepopkg}.rpm"
+  }
+
+  package{"epel-release":
     ensure   => present,
-    source   => $emi::params::emi_rel_rpm,
+    source   => "http://ftp.scientificlinux.org/linux/scientific/6x/x86_64/os/Packages/epel-release-6-8.noarch.rpm",
     provider => 'rpm',
     require => Package[$emi::params::packages_for_all]
   }
+
   
 }
